664b100bac70faf7aea52e5611ff998295d29649
Harshal-3354 <harshalkolhe0908@gmail.com>
2025-08-03 03:17:51 +0530

    Update RoleSummarizer.jsx
    changes localhost url to cloudurl

--- client/src/components/RoleSummarizer.jsx
+++ client/src/components/RoleSummarizer.jsx
@@ -13,7 +13,7 @@ const RoleSummarizer = ({ onSummarize }) => {
 
     try {
       const res = await fetch(
-        "http://localhost:5000/api/interview/summarize-role",
+        "https://vocalpath.onrender.com/api/interview/summarize-role",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
